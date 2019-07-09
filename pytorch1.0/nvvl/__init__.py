893d7850da45d290fd8015aacbdddec45a71444c
Jason (Joo Seong) Jeong <culex496782@gmail.com>
2019-07-09 15:09:50 +0000

    nvvl pytorch1.x final touch

--- pytorch1.0/nvvl/__init__.py
+++ pytorch1.0/nvvl/__init__.py
@@ -1,3 +1,5 @@
 from .dataset import VideoDataset,ProcessDesc
 from .loader import VideoLoader
 from ._nvvl import video_size_from_file
+from .rnb_dataset import RnBDataset
+from .rnb_loader import RnBLoader, Sampler
